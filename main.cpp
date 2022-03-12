579de993eb6016e240f2bb0d2aa3a9bc1b8c6d98
NasiadkaMaciej <maciej.nasiadka@gmail.com>
2022-03-12 19:48:39 +0100

    Add game menu

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "Collision.h"
 #include "entities.hpp"
+#include "menu.hpp"
 #include "textures.hpp"
 #include <SFML/Graphics.hpp>
 #include <iostream>
@@ -7,9 +8,9 @@
 #include <math.h>
 
 // game start values;
-int bigAsteroids = 6;
-int roundNum = 1;
-
+int bigAsteroids = 4; // when generating, 2 more are created
+int roundNum = 0;     // when starging, 1 is added
+bool isMenu = true;
 int
 main()
 {
@@ -27,8 +28,6 @@ main()
 
   loadTextures();
 
-  sf::Font font;
-  sf::Text text;
   font.loadFromFile("Hyperspace.otf");
   text.setFont(font);
   text.setCharacterSize(50);
@@ -41,13 +40,9 @@ main()
            0,
            0,
            &tPlayer);
-
+  Menu menu(desktopMode.width, desktopMode.height);
   std::list<Asteroid*> asteroids;
   std::list<Bullet*> bullets;
-
-  for (int i = 0; i < bigAsteroids; i++) {
-    asteroids.push_back(generateBigAsteroids(&tAsteroid));
-  }
 
   srand(time(NULL));
   sf::Clock deltaClock;
@@ -55,143 +50,163 @@ main()
   float deltaShoot;
 
   while (window.isOpen()) {
-    time += deltaTime;
-    deltaTime = deltaClock.restart();
-    deltaShoot += deltaTime.asMilliseconds();
-    sf::Event event;
-    while (window.pollEvent(event)) {
-      if (event.type == sf::Event::Closed)
-        window.close();
-
-      if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
-        window.close();
-      if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-        p.rotateRight = true;
-      else
-        p.rotateRight = false;
-      if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-        p.rotateLeft = true;
-      else
-        p.rotateLeft = false;
-      if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-        p.thrust = true;
-      else
-        p.thrust = false;
-      if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
-        p.isShooting = true;
-      else
-        p.isShooting = false;
-    }
-    if (p.isShooting)
-      if (deltaShoot >= 250) {
-        Bullet* b = new Bullet(p.x, p.y, p.angle, &tBullet);
-        bullets.push_back(b);
-        deltaShoot = 0;
-      }
-
-    // Check bullets and asteroids collisons
-    for (auto a : asteroids) {
-      for (auto b : bullets) {
-        if (Collision::PixelPerfectTest(a->sprite, b->sprite)) {
-          a->life = false;
-          b->life = false;
-          if (a->sprite.getTexture() == &tAsteroid) {
-            p.points += 20;
-            for (int i = 0; i < 2; i++) {
-              Asteroid* e = new Asteroid(a->x,
-                                         a->y,
-                                         random(6, 3),
-                                         random(6, 3),
-                                         rand() % 360,
-                                         &tAsteroidMedium);
-              asteroids.push_back(e);
-            }
-          } else if (a->sprite.getTexture() == &tAsteroidMedium) {
-            p.points += 50;
-            for (int i = 0; i < 2; i++) {
-              Asteroid* e = new Asteroid(a->x,
-                                         a->y,
-                                         random(6, 3),
-                                         random(6, 3),
-                                         rand() % 360,
-                                         &tAsteroidSmall);
-              asteroids.push_back(e);
-            }
-          } else if (a->sprite.getTexture() == &tAsteroidSmall)
-            p.points += 100;
+    if (isMenu) {
+      sf::Event event;
+      while (window.pollEvent(event)) {
+        if (event.type == sf::Event::Closed)
+          window.close();
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+          window.close();
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+          menu.move(up);
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+          menu.move(down);
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Return)) {
+          isMenu = false;
+        }
+        window.clear();
+        if (isMenu) {
+          menu.draw(window);
+          window.display();
         }
       }
-      if (Collision::PixelPerfectTest(a->sprite, p.sprite)) {
-        p.sprite.setPosition(window.getView().getCenter());
-        p.x = p.sprite.getPosition().x, p.y = p.sprite.getPosition().y;
-        p.x_speed = 0;
-        p.y_speed = 0;
-        time = sf::seconds(0);
+    } else {
+      time += deltaTime;
+      deltaTime = deltaClock.restart();
+      deltaShoot += deltaTime.asMilliseconds();
+      sf::Event event;
+      while (window.pollEvent(event)) {
+        if (event.type == sf::Event::Closed)
+          window.close();
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+          isMenu=true;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+          p.rotateRight = true;
+        else
+          p.rotateRight = false;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+          p.rotateLeft = true;
+        else
+          p.rotateLeft = false;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+          p.thrust = true;
+        else
+          p.thrust = false;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+          p.isShooting = true;
+        else
+          p.isShooting = false;
       }
-    }
-    p.update();
+      if (p.isShooting)
+        if (deltaShoot >= 250) {
+          Bullet* b = new Bullet(p.x, p.y, p.angle, &tBullet);
+          bullets.push_back(b);
+          deltaShoot = 0;
+        }
 
-    for (auto i = asteroids.begin(); i != asteroids.end();) {
-      // Entity* e = *i;
-      Asteroid* e = *i;
-      e->update();
-      if (e->life == false) {
-        i = asteroids.erase(i);
-        delete e;
-      } else
-        i++;
-    }
-    for (auto i = bullets.begin(); i != bullets.end();) {
-      Bullet* e = *i;
-      e->update();
-      if (e->life == false) {
-        i = bullets.erase(i);
-        delete e;
-      } else
-        i++;
-    }
-
-    std::string sPoints = std::to_string(p.points);
-    std::string sTime = std::to_string(time.asMilliseconds() / 10);
-    std::string sLevel = std::to_string(roundNum);
-    if (sTime.length() > 2)
-      sTime.insert(sTime.length() - 2, ".");
-    else
-      sTime = "0." + sTime;
-    text.setString(sTime + "\n" + sPoints + " points" + "\n" +
-                   "Round: " + sLevel);
-
-    if (asteroids.size() == 0) {
-      bigAsteroids += 2;
-      roundNum++;
-      for (int i = 0; i < bigAsteroids; i++) {
-        asteroids.push_back(generateBigAsteroids(&tAsteroid));
+      // Check bullets and asteroids collisons
+      for (auto a : asteroids) {
+        for (auto b : bullets) {
+          if (Collision::PixelPerfectTest(a->sprite, b->sprite)) {
+            a->life = false;
+            b->life = false;
+            if (a->sprite.getTexture() == &tAsteroid) {
+              p.points += 20;
+              for (int i = 0; i < 2; i++) {
+                Asteroid* e = new Asteroid(a->x,
+                                           a->y,
+                                           random(6, 3),
+                                           random(6, 3),
+                                           rand() % 360,
+                                           &tAsteroidMedium);
+                asteroids.push_back(e);
+              }
+            } else if (a->sprite.getTexture() == &tAsteroidMedium) {
+              p.points += 50;
+              for (int i = 0; i < 2; i++) {
+                Asteroid* e = new Asteroid(a->x,
+                                           a->y,
+                                           random(6, 3),
+                                           random(6, 3),
+                                           rand() % 360,
+                                           &tAsteroidSmall);
+                asteroids.push_back(e);
+              }
+            } else if (a->sprite.getTexture() == &tAsteroidSmall)
+              p.points += 100;
+          }
+        }
+        if (Collision::PixelPerfectTest(a->sprite, p.sprite)) {
+          p.sprite.setPosition(window.getView().getCenter());
+          p.x = p.sprite.getPosition().x, p.y = p.sprite.getPosition().y;
+          p.x_speed = 0;
+          p.y_speed = 0;
+          time = sf::seconds(0);
+        }
       }
-    }
+      p.update();
 
-    int i;
-    for (auto i = asteroids.begin(); i != asteroids.end();) {
-      Asteroid* e = *i;
-      e->update();
-      i++;
-    }
+      for (auto i = asteroids.begin(); i != asteroids.end();) {
+        // Entity* e = *i;
+        Asteroid* e = *i;
+        e->update();
+        if (e->life == false) {
+          i = asteroids.erase(i);
+          delete e;
+        } else
+          i++;
+      }
+      for (auto i = bullets.begin(); i != bullets.end();) {
+        Bullet* e = *i;
+        e->update();
+        if (e->life == false) {
+          i = bullets.erase(i);
+          delete e;
+        } else
+          i++;
+      }
 
-    for (auto i = bullets.begin(); i != bullets.end();) {
-      Bullet* e = *i;
-      e->update();
-      i++;
+      std::string sPoints = std::to_string(p.points);
+      std::string sTime = std::to_string(time.asMilliseconds() / 10);
+      std::string sLevel = std::to_string(roundNum);
+      if (sTime.length() > 2)
+        sTime.insert(sTime.length() - 2, ".");
+      else
+        sTime = "0." + sTime;
+      text.setString(sTime + "\n" + sPoints + " points" + "\n" +
+                     "Round: " + sLevel);
+
+      if (asteroids.size() == 0) {
+        bigAsteroids += 2;
+        roundNum++;
+        for (int i = 0; i < bigAsteroids; i++) {
+          asteroids.push_back(generateBigAsteroids(&tAsteroid));
+        }
+      }
+
+      int i;
+      for (auto i = asteroids.begin(); i != asteroids.end();) {
+        Asteroid* e = *i;
+        e->update();
+        i++;
+      }
+
+      for (auto i = bullets.begin(); i != bullets.end();) {
+        Bullet* e = *i;
+        e->update();
+        i++;
+      }
+      window.clear();
+      window.draw(text);
+      for (const auto& i : asteroids) {
+        i->draw(window);
+      }
+      for (const auto& i : bullets) {
+        i->draw(window);
+      }
+      p.draw(window);
+      window.display();
     }
-	
-    window.clear();
-    window.draw(text);
-    for (const auto& i : asteroids) {
-      i->draw(window);
-    }
-    for (const auto& i : bullets) {
-      i->draw(window);
-    }
-    p.draw(window);
-    window.display();
   }
   return 0;
 }
---
+++ menu.hpp
@@ -0,0 +1,69 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+const int MENU_ITEMS = 4;
+#define up 1
+#define down 2
+
+sf::Font font;
+sf::Text text;
+
+class Menu
+{
+public:
+  int selectedItemIndex = 0;
+  sf::Text menuPos[MENU_ITEMS];
+  sf::String sMenu[MENU_ITEMS]{ "Play", "Leaderboard", "Settings", "Info" };
+
+  Menu(float width, float height)
+  {
+    for (int i = 0; i < MENU_ITEMS; i++) {
+      menuPos[i].setString(sMenu[i]);
+      menuPos[i].setFont(font);
+      menuPos[i].setCharacterSize(50);
+      menuPos[i].setFillColor(sf::Color::White);
+      menuPos[i].setOrigin(menuPos[i].getGlobalBounds().width / 2,
+                           menuPos[i].getGlobalBounds().height / 2);
+      menuPos[i].setPosition(
+        sf::Vector2f(width / 2, height / (MENU_ITEMS + 1) * (i + 1)));
+    }
+    menuPos[0].setFillColor(sf::Color::Red);
+    menuPos[0].setString("> " + menuPos[0].getString() + " <");
+    menuPos[0].setOrigin(menuPos[0].getGlobalBounds().width / 2,
+                         menuPos[0].getGlobalBounds().height / 2);
+  }
+  void draw(sf::RenderWindow& window)
+  {
+    for (int i = 0; i < MENU_ITEMS; i++)
+      window.draw(menuPos[i]);
+  }
+  void move(int direction)
+  {
+    switch (direction) {
+      case up:
+        if (selectedItemIndex - 1 >= 0) {
+          menuPos[selectedItemIndex].setFillColor(sf::Color::White);
+          selectedItemIndex--;
+          menuPos[selectedItemIndex].setFillColor(sf::Color::Red);
+        }
+        break;
+      case down:
+        if (selectedItemIndex + 1 < MENU_ITEMS) {
+          menuPos[selectedItemIndex].setFillColor(sf::Color::White);
+          selectedItemIndex++;
+          menuPos[selectedItemIndex].setFillColor(sf::Color::Red);
+        }
+        break;
+    }
+    for (int i = 0; i < MENU_ITEMS; i++) {
+      menuPos[i].setString(sMenu[i]);
+      menuPos[i].setOrigin(menuPos[i].getGlobalBounds().width / 2,
+                           menuPos[i].getGlobalBounds().height / 2);
+    }
+    menuPos[selectedItemIndex].setString(
+      "> " + menuPos[selectedItemIndex].getString() + " <");
+    menuPos[selectedItemIndex].setOrigin(
+      menuPos[selectedItemIndex].getGlobalBounds().width / 2,
+      menuPos[selectedItemIndex].getGlobalBounds().height / 2);
+  };
+};
